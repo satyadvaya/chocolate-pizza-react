49d46d91728b66aea3427a65a1cb6ed823840772
satyadvaya <satyadvaya@gmail.com>
2021-08-02 19:52:13 -0700

    working Instructions component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Header from './Header.js';
 import Image from './Image.js';
+import Instructions from './Instructions';
 import Author from './Author.js';
 import Footer from './Footer.js';
 
@@ -12,6 +13,7 @@ class App extends Component {
       <>
         <Header />
         <Image />
+        <Instructions />
         <Author />
         <Footer />
       </>
